e5b3ecc1d8a832f3be08cfcaa4ca6d67b46b116a
forest <43224707+forestxieCode@users.noreply.github.com>
2019-11-19 10:06:51 +0800

    修改拦截bug

--- plugins/route.js
+++ plugins/route.js
@@ -2,7 +2,7 @@ export default ({ app ,redirect}) => {
   // 做路由拦截
   app.router.afterEach((to, from) => {
     const token = sessionStorage.getItem('token')
-    if(!(to.path==='/'||to.path==='register')){
+    if(!(to.path==='/'||to.path==='/register')){
       if(!token) redirect('/')
     }
   })
